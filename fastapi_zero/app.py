08d8a9737a5de7ab12b46ef496032128333944c9
Rubendvb <rubenvasquez556@gmail.com>
2025-06-18 00:49:46 -0300

    adding new endpoint

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,10 +1,21 @@
 from http import HTTPStatus
 from fastapi import FastAPI
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserSchema, UserPublic, UserDB
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
