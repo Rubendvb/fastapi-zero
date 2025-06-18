12e293bc26523cf37d03fa56303e153876a7a9bd
Rubendvb <rubenvasquez556@gmail.com>
2025-06-18 02:11:51 -0300

    adding test

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,23 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'password': 'secret',
+            'email': 'alice@example.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
