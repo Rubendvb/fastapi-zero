bd5d7ac2708eec4b108593e97c7e83165e7536e8
Rubendvb <rubenvasquez556@gmail.com>
2025-06-22 21:31:33 -0300

    create test for get one user

--- tests/test_app.py
+++ tests/test_app.py
@@ -41,6 +41,24 @@ def test_read_users(client):
     }
 
 
+def test_read_one_users(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
+
+
+def test_read_one_users_not_found(client):
+    response = client.get('/users/5')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found!'}
+
+
 def test_update_user(client):
     response = client.put(
         '/users/1',
@@ -73,3 +91,23 @@ def test_update_user_not_found(client):
     assert response.json() == {
         'detail': 'User not found!',
     }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+    assert response.json() == {'detail': 'User not found!'}
